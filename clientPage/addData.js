6cc4f17c95d45a2de031300dec4171c9b114ffce
ma9dalen8 <m09fischer@gmail.com>
2020-08-20 15:33:24 +0200

    i dont know 15.33

--- clientPage/addData.js
+++ clientPage/addData.js
@@ -12,3 +12,73 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibWE5ZGFsZW44IiwiYSI6ImNrYTZ4ZGdqNDBibWUyeHBuN3JmN2lrdDcifQ.SgZHAThfZLyx2Avk3th2Lg'
 }).addTo(mymap);
+
+//https://transit.router.hereapi.com/v8/routes?apiKey=yZ1g1aCLN8rvnPJdGaO697MpL44zvnU1aHx2IwgqNgA&origin=51.9568,7.6345&destination=51.9694,7.5961&modes=bus&return=intermediate
+/**function createRidesList(){
+
+  console.log(chosenPatient.rides.length);
+  for(var i=0; i<chosenPatient.rides.length; i++){
+    var ul = document.getElementById('ul');
+    var li = document.createElement('li'+i);
+    var br = document.createElement('br');
+    var checkbox = document.createElement('input');
+    var label= document.createElement("label");
+    var description = document.createTextNode(chosenPatient.rides[i]);
+
+    checkbox.type = "checkbox";
+    checkbox.id = "checkboxid" + i;
+
+    label.appendChild(checkbox);
+    label.appendChild(description);
+
+
+    //adds all elements to the website
+
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    li.appendChild(br);
+    ul.appendChild(li);
+
+  }
+}
+
+  async function fetchDatabase(){
+
+    let result =await promise();
+    //document.getElementById('databaseContent').innerHTML=JSON.stringify(result);
+    console.log(result);
+    database=result;
+    console.log(database);
+  dropDown();
+
+  }
+
+
+/**
+*@function promise
+*@desc sends a request to the server via /item
+*/
+
+function promise(){
+
+  return new Promise(function (res, req){
+    $.ajax({
+      url:"/item",
+      success: function (result){res(result);},
+      error: function (err) {console.log(err);}
+    });
+    });
+}
+
+function addToDatabase(user){
+  fetch('/save-input', {
+                            method: 'post',
+                            body: JSON.stringify(user),
+                            headers: {
+                              'Accept': 'application/json',
+                              'Content-Type': 'application/json'
+                            }
+                          });
+
+
+}
