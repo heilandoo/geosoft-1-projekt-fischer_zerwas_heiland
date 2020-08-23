cc02b8295fe569d6eb6e184627065b752fa20458
ma9dalen8 <m09fischer@gmail.com>
2020-08-23 14:00:57 +0200

    API lääääädt 14.00

--- clientPage/addData.js
+++ clientPage/addData.js
@@ -2,8 +2,10 @@
 console.log("test");
 var chosenPatient;
 var patient;
+var userCoordinates;
 var inputStops=[];
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([51.653, 10.203], 6);
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVpbGFuZG9vIiwiYSI6ImNrYWM2MTN2YjFkaTgyd3F3czRwYmRhcWcifQ.ehq-ZqczEZiBcFwaZC0jDg', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,28 +16,54 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibWE5ZGFsZW44IiwiYSI6ImNrYTZ4ZGdqNDBibWUyeHBuN3JmN2lrdDcifQ.SgZHAThfZLyx2Avk3th2Lg'
 }).addTo(mymap);
 
-function refreshDropdown(){
-  var start = document.getElementById('start');
-  var destination= document.getElementById('destination');
-  var opt1 = null;
-  var opt2 = null;
-  start.options.length=0;
-  destination.options.length=0;
+// creating a Toolbar on the Mapboxmap only option to set a marker
 
-  for(i = 0; i<inputStops.length; i++) {
-    console.log(inputStops[i].features[0].properties.name);
-    opt1 = document.createElement('option');
-    opt1.value = inputStops[i].features[0].properties.name;
-    opt1.innerHTML = inputStops[i].features[0].properties.name;
-    destination.appendChild(opt1);
 
-    opt2 = document.createElement('option');
-    opt2.value = inputStops[i].features[0].properties.name;
-    opt2.innerHTML = inputStops[i].features[0].properties.name;
-    start.appendChild(opt2);
+function setMarker(){
+  var drawnItems = new L.FeatureGroup();
+  mymap.addLayer(drawnItems);
+  var drawControl = new L.Control.Draw({
+       draw:{
+         polygon: false,
+         polyline: false,
+         line: false,
+         circle: false,
+         rectangle: false
+       },
+       edit: {
+           featureGroup: drawnItems
+       }
+   });
+   mymap.addControl(drawControl);
+// saving marker position
+   mymap.on('draw:created', function (e){
 
-    }
-}
+     var type = e.layerType,
+     layer = e.layer;
+     userCoordinates=(layer.getLatLng());
+     if (type === 'marker') {
+       mymap.on('click', function(e) {
+    }),
+    layer.bindPopup('Your chosen Location: '+ layer.getLatLng()).openPopup();}
+
+
+    inputMarker=drawnItems.addLayer(layer);
+    userCoordinates.lat=(userCoordinates.lat).toFixed(3);
+    userCoordinates.lng=(userCoordinates.lng).toFixed(3);
+
+    let userLocation = {"type":"FeatureCollection",
+      "features":[
+          {"type": "Feature",
+          "geometry":{"type": "Point", "coordinates":[ userCoordinates.lng, userCoordinates.lat]},
+          "properties":{ "name":"Marker: " +JSON.stringify(userCoordinates)}}]};
+
+    console.log(userCoordinates);
+    inputStops.push(userLocation);
+    refreshDropdown();
+});
+   }
+
+
 function getLocation(){
   if (navigator.geolocation){
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -47,12 +75,16 @@ function getLocation(){
 }
 
 function showPosition(position) {
+
     let userLocation = {"type":"FeatureCollection",
       "features":[
           {"type": "Feature",
           "geometry":{"type": "Point", "coordinates":[ position.coords.longitude, position.coords.latitude]},
           "properties":{ "name":"aktuelle Position"}}]};
     //JSON.parse('{"userLocation":true}');
+    userLocation.features[0].geometry.coordinates[0]=(userLocation.features[0].geometry.coordinates[0]).toFixed(3);
+    userLocation.features[0].geometry.coordinates[1]=(userLocation.features[0].geometry.coordinates[1]).toFixed(3);
+    console.log(userLocation.features[0].geometry.coordinates);
     inputStops.push(userLocation);
     //document.getElementById('browserLocation').innerHTML=JSON.stringify(userLocation);
     //console.log(userLocation);
@@ -85,19 +117,19 @@ function geocoding(){
   var resource ="https://api.mapbox.com/geocoding/v5/mapbox.places/"+ street+ "%20"+ nr +"%20" + city +".json?country=DE&access_token="+access_token;
   console.log(resource);
   //if (inputMarker != undefined){inputMarker.remove();}
-    g.onload = loadcallback;
-    g.onerror = errorcallback;
-    g.onreadystatechange = statechangecallback;
+    g.onload = gloadcallback;
+    g.onerror = gerrorcallback;
+    g.onreadystatechange = gstatechangecallback;
     g.open("GET", resource, true);
     g.send();
 
 }
 
 /**
-*@function statechangecallback
+*@function gstatechangecallback
 *@desc checking if the XMLHttpRequest is in the correct form, calls mappingUserInput function
 */
-function statechangecallback() {
+function gstatechangecallback() {
   if (g.status == "200" && g.readyState == 4) {
 
     convertedAdress = g.responseText;
@@ -105,13 +137,115 @@ function statechangecallback() {
     let userAdress = {"type":"FeatureCollection",
       "features":[
           {"type": "Feature",
-          "geometry":{"type": "Point", "coordinates":[ convertedAdress.features[0].geometry.coordinates]},
+          "geometry":{"type": "Point", "coordinates": convertedAdress.features[0].geometry.coordinates},
           "properties":{ "name":"Adresse,"+JSON.stringify(convertedAdress.query[0]+' '+ convertedAdress.query[1]+' '+convertedAdress.query[2])}}]};
-    console.log(convertedAdress);
-    console.log(convertedAdress.features[0].geometry.coordinates);
+          console.log(userAdress.features[0].geometry.coordinates[0]);
+          userAdress.features[0].geometry.coordinates[0]=(userAdress.features[0].geometry.coordinates[0]).toFixed(3);
+          userAdress.features[0].geometry.coordinates[1]=(userAdress.features[0].geometry.coordinates[1]).toFixed(3);
     inputStops.push(userAdress);
     //convertedAdress=convertedAdress.features[0].geometry.coordinates;
     refreshDropdown();
+    }
+}
+
+/**
+*@function gerrorcallback
+*@desc informs the User about an error
+*/
+function gerrorcallback(e) {
+  document.getElementById("error").innerHTML = "errorcallback: check web-console";
+}
+
+/**
+*@function gloadcallback
+*@desc informs about an incorrect format in the console
+*/
+function gloadcallback() {
+  if(g.status!="200"){
+    console.log(g.status);
+  }
+}
+
+function refreshDropdown(){
+  var start = document.getElementById('start');
+  var destination= document.getElementById('destination');
+  var opt1 = null;
+  var opt2 = null;
+  start.options.length=0;
+  destination.options.length=0;
+
+  for(i = 0; i<inputStops.length; i++) {
+    console.log(inputStops[i].features[0].properties.name);
+    opt1 = document.createElement('option');
+    opt1.value = inputStops[i].features[0].properties.name;
+    opt1.innerHTML = inputStops[i].features[0].properties.name;
+    destination.appendChild(opt1);
+
+    opt2 = document.createElement('option');
+    opt2.value = inputStops[i].features[0].properties.name;
+    opt2.innerHTML = inputStops[i].features[0].properties.name;
+    start.appendChild(opt2);
+
+    }
+}
+
+function creatingApi(){
+  console.log(inputStops);
+  var startInput=document.getElementById('start').value;
+  var destiInput=document.getElementById('destination').value;
+  var dateInput=document.getElementById('start-date').value;
+  var timeInput=document.getElementById('start-time').value;
+  var apiKey='yZ1g1aCLN8rvnPJdGaO697MpL44zvnU1aHx2IwgqNgA';
+  var api;
+  var startCoordinates=[];
+  var destiCoordinates=[];
+
+
+  if(startInput=='' | destiInput=='' | dateInput=='' | timeInput==''){
+    alert('Eingaben unvollständig');
+    return;
+  }
+  if(startInput==destiInput){ alert('Startpunkt und Ziel sind identisch');return;}
+  else{
+    for(var i=0; i<inputStops.length; i++){
+      if(inputStops[i].features[0].properties.name==startInput){
+        startCoordinates.push(inputStops[i].features[0].geometry.coordinates[1]);
+        startCoordinates.push(inputStops[i].features[0].geometry.coordinates[0]);
+        console.log(startCoordinates[0]+','+startCoordinates[1]);
+      }
+      if(inputStops[i].features[0].properties.name==destiInput){
+        destiCoordinates.push(inputStops[i].features[0].geometry.coordinates[1]);
+        destiCoordinates.push(inputStops[i].features[0].geometry.coordinates[0]);
+      }
+      console.log(inputStops);
+       api='https://transit.router.hereapi.com/v8/routes?apiKey='+apiKey+'&origin='+startCoordinates[0]+','+startCoordinates[1]+'&destination='+destiCoordinates[0]+','+destiCoordinates[1]+'&departureTime='+dateInput+'T'+timeInput+':00&return=intermediate';
+       console.log(api);
+       fetchingRoute(api);
+    }
+
+  }
+}
+var x = new XMLHttpRequest();
+var route;
+
+function fetchingRoute(api){
+    x.onload = loadcallback;
+    x.onerror = errorcallback;
+    x.onreadystatechange = statechangecallback;
+    x.open("GET", api, true);
+    x.send();
+}
+
+/**
+*@function statechangecallback
+*@desc checking if the XMLHttpRequest is in the correct form, calls mappingUserInput function
+*/
+function statechangecallback() {
+  if (x.status == "200" && x.readyState == 4) {
+
+    route = x.responseText;
+    console.log(route);
+
     }
 }
 
@@ -120,7 +254,7 @@ function statechangecallback() {
 *@desc informs the User about an error
 */
 function errorcallback(e) {
-  document.getElementById("result").innerHTML = "errorcallback: check web-console";
+  document.getElementById("error").innerHTML = "errorcallback: check web-console";
 }
 
 /**
@@ -129,7 +263,7 @@ function errorcallback(e) {
 */
 function loadcallback() {
   if(g.status!="200"){
-    console.log(g.status);
+    console.log(x.status);
   }
 }
 //https://transit.router.hereapi.com/v8/routes?apiKey=yZ1g1aCLN8rvnPJdGaO697MpL44zvnU1aHx2IwgqNgA&origin=51.9568,7.6345&destination=51.9694,7.5961&modes=bus&return=intermediate
